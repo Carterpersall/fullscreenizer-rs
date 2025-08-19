b4ed7ab73a6a06e0803cfdd0e169f83f2c7bfad5
Carterpersall <carterpersall@gmail.com>
2025-08-19 13:28:05 -0500

    Add embed_resource for dynamic .res compilation
    Replaced static resources.res with dynamic compilation using embed-resource in build.rs. This change fixes the linking issues that made the GNU toolchain not work, and also makes it a lot easier to modify the resource file  in the future.

--- build.rs
+++ build.rs
@@ -1,25 +1,7 @@
 fn main() {
-    // Tell the linker to include the resources.res file
-    // The resource file contains the icon and manifest
-    // The manifest does the following:
-    //  1. requestedExecutionLevel:
-    //      - level = "asInvoker"
-    //          - The application will run with the same privileges as the user who started it.
-    //      - uiAccess = "false"
-    //          - The application will not be able to access protected UI elements.
-    //  2. supportedOS:
-    //      - Id = "{8e0f7a12-bfb3-4fe8-b9a5-48fd50a15a9a}"
-    //          - Windows 10 and later
-    //  3. assemblyIdentity:
-    //      - name = "Microsoft.Windows.Common-Controls"
-    //          - The application will use the common controls library.
-    //          - This is required for the application to use the common controls (like buttons, edit boxes, etc.)
-    //  4. windowsSettings:
-    //      - dpiAware = "true/pm"
-    //          - The application will be DPI aware on a per-monitor basis.
-    //          - Only applies before Windows 10 1607.
-    //      - dpiAwareness = "permonitorV2,permonitor"
-    //          - The application will be DPI aware on a per-monitor basis.
-    //          - Windows 10 1607 and later.
-    println!("cargo:rustc-link-lib=dylib:+verbatim=resources/resources.res");
+    // Compile the resources.rc file into a .res file
+    embed_resource::compile("resources/resources.rc", embed_resource::NONE)
+        .manifest_required().unwrap();
+    // Rerun when anything under ./resources/ changes
+    println!("cargo:rerun-if-changed=resources");
 }
